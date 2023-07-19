500b82ec69d4a74a9c311c8a37837e3d55727055
Emanuel Bodin <bodinemanuel@gmail.com>
2023-07-19 21:06:56 +0200

    fixed some things

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,13 @@
+import { fail } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 import db from '$lib/db';
 import { todoSchema } from '$lib/db/schema';
 export const actions = {
 	create: async ({ request }) => {
 		const data = await request.formData();
-		const title = data.get('title') as string;
-		await db.insert(todoSchema).values({ title, completed: false }).run();
+		const title = data.get('title');
+		if (!title) return fail(422, { title, error: 'Title is required' });
+		await db.insert(todoSchema).values({ title: title.toString(), completed: false }).run();
 	},
 	delete: async ({ request }) => {
 		const data = await request.formData();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	export let data;
+	export let form;
 </script>
 
 <svelte:head>
@@ -10,9 +11,12 @@
 <section class="flex flex-col justify-center align-center">
 
 	<h1>Todos</h1>
+	{#if form?.error}
+	<p class="text-red">{form.error}</p>
+	{/if}
 	<form class="mb-4" method="post" action="?/create">
 		<label for="title">add a todo:</label>
-		<input name="title" autocomplete="off"/>
+		<input name="title" autocomplete="off" required value={form?.title ?? ''}/>
 	</form>
 
 	<h2>
@@ -21,9 +25,12 @@
 	{#each data.todos as {id,title, completed} (id)}
 		<div class="flex gap-1">
 		<form method="post" action="?/delete">
+			<div class="flex gap-2 items-center">
 			<input type="hidden" name="id" value={id}/>
-			<a href="/todo/{id}">{title}</a>
-			<button aria-label="Mark as complete">Complete</button>
+			<a class="text-black text-lg" href="/todo/{id}">{title}</a>
+			<p>Status:<p><p class='text-green-700'>{completed ? 'Completed' : 'In progress'}</p>
+			<button class="bg-stone-900 text-white p-2 rounded" aria-label="Mark as complete">Complete</button>
+		</div>
 		</form>
 		</div>
 	{/each}
